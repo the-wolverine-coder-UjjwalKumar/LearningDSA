308072b86833a4556f76848eba443e2098230304
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-01-18 23:09:00 +0530

    Added leetcode solutions and BT left right top bottom view

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/LeetCodeQuestions/Node2.java
@@ -0,0 +1,18 @@
+package com.ujjwal_Learning.LeetCodeQuestions;
+
+public class Node2 {
+
+	Node2 node;
+	int hd;
+	Node2 left;
+	Node2 right;
+	public Integer data;
+
+	public Node2(Node2 node, int hd, Node2 left, Node2 right, int data) {
+		this.node = node;
+		this.hd = hd;
+		this.left = left;
+		this.right = right;
+		this.data = data;
+	}
+}
