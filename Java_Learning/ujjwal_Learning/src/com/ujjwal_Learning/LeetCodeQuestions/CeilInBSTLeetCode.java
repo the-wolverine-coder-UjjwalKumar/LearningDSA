adcc8a2aab19f382b7ec1e9cbea9afc996b8222c
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-01-24 23:36:59 +0530

    Added BST question

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/LeetCodeQuestions/CeilInBSTLeetCode.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/LeetCodeQuestions/CeilInBSTLeetCode.java
@@ -6,5 +6,25 @@ public class CeilInBSTLeetCode {
 		// TODO Auto-generated method stub
 
 	}
+	
+	 public  static int findCeil(TreeNode root, int key) {
 
+	        int ceil = -1; 
+	        while (root != null) {
+	            if (root.val == key) {
+	                ceil = root.val;
+	                return ceil;
+	            }
+
+	            if (key > root.val) {
+	                root = root.right;
+	            }
+	            else {
+	                ceil = root.val; 
+	                root = root.left;
+	            }
+	        }
+	        return ceil;
+
+	 }
 }
