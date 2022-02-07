070bf62b0eae1c943c979a3dfb164aaac5ff7d6a
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-02-07 13:16:51 +0530

    Updated topological sorting using BFS Kahn's Algorithm

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/graph/TopologicalSortUsingBFSKahnsAlgorithm.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/graph/TopologicalSortUsingBFSKahnsAlgorithm.java
@@ -85,5 +85,36 @@ public class TopologicalSortUsingBFSKahnsAlgorithm {
 
 		return ans;
 	}
+	
+	public int[] getTopoSortKhansAlgorithmBFS(int N, ArrayList<ArrayList<Integer>> adj) {
+        int topo[] = new int[N]; 
+        int indegree[] = new int[N]; 
+        for(int i = 0;i<N;i++) {
+            for(Integer it: adj.get(i)) {
+                indegree[it]++; 
+            }
+        }
+        
+        Queue<Integer> q = new LinkedList<Integer>(); 
+        for(int i = 0;i<N;i++) {
+            if(indegree[i] == 0) {
+                q.add(i); 
+            }
+        }
+       
+        int ind = 0;
+        while(!q.isEmpty()) {
+            Integer node = q.poll(); 
+            topo[ind++] = node;
+            for(Integer it: adj.get(node)) {
+                indegree[it]--; 
+                if(indegree[it] == 0) {
+                    q.add(it); 
+                }
+            }
+        }
+        
+       return topo;
+    }
 
 }
