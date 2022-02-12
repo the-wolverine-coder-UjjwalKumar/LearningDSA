8b2fab7cc0cb46f4f9c5b78cc8a23a306f4a8c5d
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-02-12 22:51:25 +0530

    Added recursive solution

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/LeetCodeQuestions/ReverseStringLeetCode344.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/LeetCodeQuestions/ReverseStringLeetCode344.java
@@ -6,7 +6,7 @@ public class ReverseStringLeetCode344 {
 		// TODO Auto-generated method stub
 		
 		char[]	s = {'h','e','l','l','o'};
-		reverseString(s);
+		reverseString2(s);
 		System.out.println(new String(s));
 	}
 
@@ -30,5 +30,29 @@ public class ReverseStringLeetCode344 {
 		s[i] = s[j];
 		s[j] = t;
 	}
+	
+    public static void reverseString2(char[] s){
+        
+        int i = 0;
+        int j = s.length -1;
+        
+        char[] ans = helper(s, i, j);
+        System.out.println(ans);
+
+    }
+    
+    public static char[] helper(char[] s, int i, int  j){
+        if(i>=j) return s;
+        
+        int n = s.length;
+        if(i<n && j<n){
+            swap(s,i,j);
+            i++;
+            j--;
+            
+            return helper(s,i,j);
+        }
+        return s;
+    }
 
 }
