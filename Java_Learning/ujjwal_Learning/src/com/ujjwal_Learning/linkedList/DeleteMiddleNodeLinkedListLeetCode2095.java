00fda84ee97e838831e18c90c3dea2545fe4116d
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-02-13 23:19:50 +0530

    Added LeetCode solution 2095

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/DeleteMiddleNodeLinkedListLeetCode2095.java
@@ -0,0 +1,33 @@
+package com.ujjwal_Learning.linkedList;
+
+public class DeleteMiddleNodeLinkedListLeetCode2095 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode deleteMiddle(ListNode head) {
+		if (head == null)
+			return head;
+
+		ListNode fast = head;
+		ListNode slow = head;
+		ListNode prevMid = null; // carrying the previous to middle node
+
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			prevMid = slow;
+			slow = slow.next;
+		}
+
+		if (prevMid == null)
+			return null;
+
+		prevMid.next = slow.next;
+
+		return head;
+
+	}
+
+}
