67e08398f14735367238fd09f31037b3cc9d0d70
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-02-17 22:46:40 +0530

    Added LeetCode solution 234

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/PalinDromeLinkedListLeetCode234.java
@@ -0,0 +1,43 @@
+package com.ujjwal_Learning.linkedList;
+
+public class PalinDromeLinkedListLeetCode234 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isPalindrome(ListNode head) {
+		ListNode fast = head, slow = head;
+		while (fast != null && fast.next != null) {
+			fast = fast.next.next;
+			slow = slow.next;
+		}
+		if (fast != null) { // odd nodes: let right half smaller
+			slow = slow.next;
+		}
+		slow = reverse(slow);
+		fast = head;
+
+		while (slow != null) {
+			if (fast.val != slow.val) {
+				return false;
+			}
+			fast = fast.next;
+			slow = slow.next;
+		}
+		return true;
+	}
+
+	public ListNode reverse(ListNode head) {
+		ListNode prev = null;
+		while (head != null) {
+			ListNode next = head.next;
+			head.next = prev;
+			prev = head;
+			head = next;
+		}
+		return prev;
+	}
+
+}
