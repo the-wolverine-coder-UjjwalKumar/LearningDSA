dce24c12ed946fe3af799baad4bb78ce9f8ef1c0
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-02-18 22:27:54 +0530

    Updated LeetCode solution 142

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/CycleInLinkedListTwoLeetCode142.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/CycleInLinkedListTwoLeetCode142.java
@@ -23,4 +23,24 @@ public class CycleInLinkedListTwoLeetCode142 {
 		}
 		return head;
 	}
+
+	// better approach
+	public ListNode removeNthFromEnd(ListNode head, int n) {
+		ListNode start = new ListNode();
+		start.next = head;
+		ListNode fast = start;
+		ListNode slow = start;
+
+		for (int i = 1; i <= n; ++i)
+			fast = fast.next;
+
+		while (fast.next != null) {
+			fast = fast.next;
+			slow = slow.next;
+		}
+
+		slow.next = slow.next.next;
+
+		return start.next;
+	}
 }
