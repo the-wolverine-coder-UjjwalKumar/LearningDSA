679f624d8a6ce66d1ebcb0926603b6007fa4a5d5
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-02-22 23:19:01 +0530

    Added LeetCode solution 46

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/recusrion/PermutationLeetCode46.java
@@ -0,0 +1,34 @@
+package com.ujjwal_Learning.recusrion;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PermutationLeetCode46 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<List<Integer>> permute(int[] nums) {
+		List<List<Integer>> list = new ArrayList<>();
+		// Arrays.sort(nums); // not necessary
+		backtrack(list, new ArrayList<>(), nums);
+		return list;
+	}
+
+	private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] nums) {
+		if (tempList.size() == nums.length) {
+			list.add(new ArrayList<>(tempList));
+		} else {
+			for (int i = 0; i < nums.length; i++) {
+				if (tempList.contains(nums[i]))
+					continue; // element already exists, skip
+				tempList.add(nums[i]);
+				backtrack(list, tempList, nums);
+				tempList.remove(tempList.size() - 1);
+			}
+		}
+	}
+
+}
