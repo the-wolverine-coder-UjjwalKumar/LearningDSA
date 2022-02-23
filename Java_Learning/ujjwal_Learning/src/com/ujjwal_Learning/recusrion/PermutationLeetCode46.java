7c924469b8b0a5c41d94d146aad3d78e1ce2a908
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-02-23 22:40:41 +0530

    Updated Permutation of Arrays/String LeetCode 46

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/recusrion/PermutationLeetCode46.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/recusrion/PermutationLeetCode46.java
@@ -30,5 +30,33 @@ public class PermutationLeetCode46 {
 			}
 		}
 	}
+	
+	//methods using swap concept
+	private void recurPermute(int index, int[] nums, List<List<Integer>> ans) {
+        if(index == nums.length) {
+            // copy the ds to ans
+            List<Integer> ds = new ArrayList<>();
+            for(int i = 0;i<nums.length;i++) {
+                ds.add(nums[i]); 
+            }
+            ans.add(new ArrayList<>(ds)); 
+            return; 
+        }
+        for(int i = index;i<nums.length;i++) {
+            swap(i, index, nums); 
+            recurPermute(index+1, nums, ans); 
+            swap(i, index, nums); 
+        }
+    }
+    private void swap(int i, int j, int[] nums) {
+        int t = nums[i]; 
+        nums[i] = nums[j]; 
+        nums[j] = t;
+    }
+    public List<List<Integer>> permute2(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>(); 
+        recurPermute(0, nums, ans);
+        return ans; 
+    }
 
 }
