6ec8ccea2e313bcce009d05f785ad5fbe3f74759
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-05 22:50:01 +0530

    Added leetcode solution 45

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming/JumpGameTwoLeetCode45.java
@@ -0,0 +1,10 @@
+package com.ujjwal_Learning.dynamicProgramming;
+
+public class JumpGameTwoLeetCode45 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
