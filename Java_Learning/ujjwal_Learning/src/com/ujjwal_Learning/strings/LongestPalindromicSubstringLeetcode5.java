b48e44d7ddb7224ef3df1b783e73dcc74ca5c669
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-18 22:33:31 +0530

    Added Leetcode solution 5

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/LongestPalindromicSubstringLeetcode5.java
@@ -0,0 +1,29 @@
+package com.ujjwal_Learning.strings;
+
+public class LongestPalindromicSubstringLeetcode5 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String longestPalindrome(String s) {
+		int n = s.length();
+		String res = null;
+
+		boolean[][] dp = new boolean[n][n];
+
+		for (int i = n - 1; i >= 0; i--) {
+			for (int j = i; j < n; j++) {
+				dp[i][j] = s.charAt(i) == s.charAt(j) && (j - i < 3 || dp[i + 1][j - 1]);
+
+				if (dp[i][j] && (res == null || j - i + 1 > res.length())) {
+					res = s.substring(i, j + 1);
+				}
+			}
+		}
+
+		return res;
+	}
+
+}
