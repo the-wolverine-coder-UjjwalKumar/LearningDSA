ff34e37dd564ce76df75f52fe9971f8009129707
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-18 22:31:24 +0530

    Added Leetcode solution 1512

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays/GoodPairsLeetCode1512.java
@@ -0,0 +1,18 @@
+package com.ujjwal_Learning.Arrays;
+
+public class GoodPairsLeetCode1512 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int numIdenticalPairs(int[] nums) {
+		int res = 0, count[] = new int[101];
+		for (int a : nums) {
+			res += count[a]++;
+		}
+		return res;
+	}
+
+}
