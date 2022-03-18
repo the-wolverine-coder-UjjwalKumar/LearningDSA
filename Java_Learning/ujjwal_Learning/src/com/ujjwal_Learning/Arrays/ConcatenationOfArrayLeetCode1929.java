4dbe3fde87bccf3eb3feaf234bfe9672bcd55501
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-18 22:30:35 +0530

    Added leetcode solution 1929

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays/ConcatenationOfArrayLeetCode1929.java
@@ -0,0 +1,23 @@
+package com.ujjwal_Learning.Arrays;
+
+public class ConcatenationOfArrayLeetCode1929 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int[] getConcatenation(int[] arr) {
+		int n = arr.length;
+
+		int[] ans = new int[n * 2];
+
+		for (int i = 0; i < n; i++) {
+			ans[i] = arr[i];
+			ans[i + n] = arr[i];
+		}
+
+		return ans;
+	}
+
+}
