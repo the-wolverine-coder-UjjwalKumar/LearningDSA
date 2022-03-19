46f25ab3048a80ff97fcf8beb41d877209c9969b
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-19 09:08:50 +0530

    Added Leetcode solution 334

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays/IncreasingTripletSubsequenceLeetcode334.java
@@ -0,0 +1,22 @@
+package com.ujjwal_Learning.Arrays;
+
+public class IncreasingTripletSubsequenceLeetcode334 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean increasingTriplet(int[] nums) {
+		int first = Integer.MAX_VALUE, sec = Integer.MAX_VALUE;
+		for (int j : nums)
+			if (j <= first)
+				first = j;
+			else if (j <= sec)
+				sec = j;
+			else
+				return true;
+		return false;
+	}
+
+}
