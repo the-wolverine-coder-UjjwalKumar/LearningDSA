32bbe61603d94d85e4b3c7a917f6a7c3495fcbcc
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-21 21:13:54 +0530

    Added Leetcode solution 27

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays/RemoveElementsLeetcode27.java
@@ -0,0 +1,23 @@
+package com.ujjwal_Learning.Arrays;
+
+public class RemoveElementsLeetcode27 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int removeElement(int[] A, int elem) {
+		int m = 0;
+		for (int i = 0; i < A.length; i++) {
+
+			if (A[i] != elem) {
+				A[m] = A[i];
+				m++;
+			}
+		}
+
+		return m;
+	}
+
+}
