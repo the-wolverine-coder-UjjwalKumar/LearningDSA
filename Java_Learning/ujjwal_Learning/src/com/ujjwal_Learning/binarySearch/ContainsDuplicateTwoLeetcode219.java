41dbe73ded539ae525d211214acb61932c503bda
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-22 23:33:42 +0530

    Added leetcode solution 219

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/ContainsDuplicateTwoLeetcode219.java
@@ -0,0 +1,26 @@
+package com.ujjwal_Learning.binarySearch;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ContainsDuplicateTwoLeetcode219 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean containsNearbyDuplicate(int[] nums, int k) {
+
+		int n = nums.length;
+		Set<Integer> set = new HashSet<>();
+		for (int i = 0; i < n; i++) {
+			if (!set.add(nums[i]))
+				return true;
+			if (set.size() > k)
+				set.remove(nums[i - k]);
+		}
+		return false;
+	}
+
+}
