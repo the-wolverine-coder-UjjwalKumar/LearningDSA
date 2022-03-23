481fc01ac0ce8c2b4930f42176dacaf5aa789b3b
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-23 22:43:00 +0530

    Added Leetcode solution 650

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays/Key2KeyboardLeetcode650.java
@@ -0,0 +1,21 @@
+package com.ujjwal_Learning.Arrays;
+
+public class Key2KeyboardLeetcode650 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int minSteps(int n) {
+		int res = 0;
+		for (int i = 2; i <= n; i++) {
+			while (n % i == 0) {
+				res += i;
+				n = n / i;
+			}
+		}
+		return res;
+	}
+
+}
