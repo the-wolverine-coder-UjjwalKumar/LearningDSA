269402cf9b811a84295b9589698d65c8b2a3c946
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-26 23:58:10 +0530

    Added Leetcode solution 240

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/matrixProblems/SearchIn2DMatrixTwoLeetcode240.java
@@ -0,0 +1,32 @@
+package com.ujjwal_Learning.matrixProblems;
+
+public class SearchIn2DMatrixTwoLeetcode240 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean searchMatrix(int[][] matrix, int target) {
+		// Starting from last element of first row (can also start from first element of
+		// last row).
+		int i = 0;
+		int j = matrix[0].length - 1;
+
+		while (j >= 0 && i < matrix.length) {
+			if (matrix[i][j] < target) {
+				i++;
+			} else if (matrix[i][j] > target) {
+				j--;
+			}
+			// Found the element, return true.
+			else {
+				return true;
+			}
+		}
+
+		// Element not found, return false.
+		return false;
+	}
+
+}
