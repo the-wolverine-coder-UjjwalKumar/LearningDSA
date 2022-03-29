701d2bf70f01fe7f010427d169b9f75b2dd07a1b
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-03-29 23:29:04 +0530

    Added leetcode solution 1678

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/GoalParseLeetcode1678.java
@@ -0,0 +1,16 @@
+package com.ujjwal_Learning.strings;
+
+public class GoalParseLeetcode1678 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String interpret(String command) {
+		String res = command.replace("()", "o");
+		String res2 = res.replace("(al)", "al");
+		return res2;
+	}
+
+}
