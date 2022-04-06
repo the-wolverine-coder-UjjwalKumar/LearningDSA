21465514aaf7a28b1445be2d4a4a132e13f6e6a7
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-04-06 22:37:10 +0530

    Added Leetcode solution 15

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays2/ThreeSumLeetcode15.java
@@ -0,0 +1,37 @@
+package com.ujjwal_Learning.Arrays2;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ThreeSumLeetcode15 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<List<Integer>> threeSum(int[] nums) {
+		Set<List<Integer>> set = new HashSet<>();
+		if (nums.length == 0)
+			return new ArrayList<>(set);
+		Arrays.sort(nums);
+		for (int i = 0; i < nums.length - 2; i++) {
+			int j = i + 1; // j starts at i+1
+			int k = nums.length - 1; // k starts at the end
+			while (j < k) {
+				int sum = nums[i] + nums[j] + nums[k];
+				if (sum == 0)
+					set.add(Arrays.asList(nums[i], nums[j++], nums[k--]));
+				else if (sum > 0)
+					k--;
+				else if (sum < 0)
+					j++;
+			}
+		}
+		return new ArrayList<>(set);
+	}
+
+}
