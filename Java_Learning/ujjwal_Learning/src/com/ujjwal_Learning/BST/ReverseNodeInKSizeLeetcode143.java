3ccbadb3f582f588844d487b938887d0c067a8fd
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-04-17 22:02:45 +0530

    Updated Leetcode solution 143

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/BST/ReverseNodeInKSizeLeetcode143.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/BST/ReverseNodeInKSizeLeetcode143.java
@@ -5,6 +5,14 @@ import java.util.Stack;
 import com.ujjwal_Learning.linkedList.ListNode;
 
 public class ReverseNodeInKSizeLeetcode143 {
+	
+	 public class ListNode {
+		      int val;
+		      ListNode next;
+		      ListNode() {}
+		      ListNode(int val) { this.val = val; }
+		      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+	}
 
 	public void reorderList(ListNode head) {
 		int n = 0;
