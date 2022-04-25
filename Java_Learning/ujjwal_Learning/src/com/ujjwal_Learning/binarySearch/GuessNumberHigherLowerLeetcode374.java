f7476d59ed94462012221d8652ae7290125275ed
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-04-25 22:04:56 +0530

    Added Leetcode solution 374

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/GuessNumberHigherLowerLeetcode374.java
@@ -0,0 +1,32 @@
+package com.ujjwal_Learning.binarySearch;
+
+public class GuessNumberHigherLowerLeetcode374 {
+	int pickedNum = 6;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public int guessNumber(int n) {
+		int l = 1, r = n;
+		while (l <= r) {
+			int mid = l + (r - l) / 2;
+			if (guess(mid) == 0)
+				return mid;
+			else if (guess(mid) == 1)
+				l = mid + 1;
+			else
+				r = mid - 1;
+		}
+
+		return -1;
+	}
+	
+	public int guess(int n) {
+		if (n>pickedNum) return -1;
+		else if(n<pickedNum) return 1;
+		else return 0;
+	}
+
+}
