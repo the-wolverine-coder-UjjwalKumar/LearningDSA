cf09a5ef0b96adb52a9cb8a9fb89412dc4b7b64b
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-06 22:54:40 +0530

    Added Leetcode solution 127

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/WordLadder1Leetcode127.java
@@ -0,0 +1,50 @@
+package com.ujjwal_Learning.strings;
+
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+import java.util.Set;
+
+public class WordLadder1Leetcode127 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int ladderLength(String bword, String eword, List<String> list) {
+		Set<String> unvisited = new HashSet<>(list); // set to check if endWord exist & also used to keep track of
+														// visited words
+		if (!unvisited.contains(eword))
+			return 0;
+
+		Queue<String> queue = new LinkedList<>();
+		queue.offer(bword);
+		unvisited.remove(bword);
+		int res = 1;
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			for (int i = 0; i < size; i++) {
+				String pword = queue.poll(); // remove all words of x bfs level
+				if (eword.equals(pword))
+					return res;
+				for (int j = 0; j < pword.length(); j++) { // TC: O(wordLen)
+					for (char k = 'a'; k <= 'z'; k++) { // TC: O(26)
+						char[] charArr = pword.toCharArray();
+						charArr[j] = k;
+						String nword = new String(charArr);
+						if (unvisited.contains(nword)) {
+							queue.offer(nword);
+							unvisited.remove(nword);
+						}
+					}
+				}
+			}
+			res++;
+		}
+
+		return 0;
+	}
+
+}
