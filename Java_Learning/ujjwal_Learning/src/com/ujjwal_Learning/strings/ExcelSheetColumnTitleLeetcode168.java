082fa0244baea34e861a251da48f0f6a003abc1c
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-12 22:22:03 +0530

    Added Leetcode solution 168

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/ExcelSheetColumnTitleLeetcode168.java
@@ -0,0 +1,24 @@
+package com.ujjwal_Learning.strings;
+
+public class ExcelSheetColumnTitleLeetcode168 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String convertToTitle(int columnNumber) {
+		int n = columnNumber;
+
+		StringBuilder result = new StringBuilder();
+
+		while (n > 0) {
+			n--;
+			result.insert(0, (char) ('A' + n % 26));
+			n /= 26;
+		}
+
+		return result.toString();
+	}
+
+}
