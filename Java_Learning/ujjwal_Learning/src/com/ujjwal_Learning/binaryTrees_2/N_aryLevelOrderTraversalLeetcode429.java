d2f11d98c396ea656f299adb94b66bc8638b71e3
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-14 22:19:23 +0530

    Added Leetcode solution 429

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/N_aryLevelOrderTraversalLeetcode429.java
@@ -0,0 +1,40 @@
+package com.ujjwal_Learning.binaryTrees_2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import com.ujjwal_Learning.binaryTrees_2.MaximumHeightNaryTreeLeetCode559.Node;
+
+public class N_aryLevelOrderTraversalLeetcode429 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<List<Integer>> levelOrder(Node root) {
+		if (root == null) {
+			return Collections.emptyList();
+		}
+		Queue<Node> nodes = new LinkedList<>();
+		List<List<Integer>> values = new ArrayList<>();
+		nodes.offer(root);
+		while (!nodes.isEmpty()) {
+			int levelSize = nodes.size();
+			List<Integer> value = new ArrayList<>(levelSize);
+			for (int i = 0; i < levelSize; i++) {
+				Node current = nodes.poll();
+				for (Node n : current.children) {
+					nodes.offer(n);
+				}
+				value.add(current.val);
+			}
+			values.add(value);
+		}
+		return values;
+	}
+
+}
