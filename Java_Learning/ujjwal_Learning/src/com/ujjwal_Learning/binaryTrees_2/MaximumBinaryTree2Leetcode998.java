23dc01c66353ba8889147ce6551cdb481ca884aa
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-15 09:08:41 +0530

    Added Leetcode solution 998

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/MaximumBinaryTree2Leetcode998.java
@@ -0,0 +1,28 @@
+package com.ujjwal_Learning.binaryTrees_2;
+
+public class MaximumBinaryTree2Leetcode998 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+		if (root == null)
+			return null;
+
+		if (root.val < val) {
+			TreeNode node = new TreeNode(val);
+			node.left = root;
+			return node;
+		} else {
+			if (root.right != null) {
+				root.right = insertIntoMaxTree(root.right, val);
+			} else {
+				root.right = new TreeNode(val);
+			}
+		}
+		return root;
+	}
+
+}
