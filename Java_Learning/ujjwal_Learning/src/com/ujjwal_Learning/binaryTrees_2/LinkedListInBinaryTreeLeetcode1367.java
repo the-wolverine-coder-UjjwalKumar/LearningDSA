69a96de42b9f7d4c328dc544d05309f9d187ad99
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-05-28 22:07:24 +0530

    Added Leetcode solution 1367

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/LinkedListInBinaryTreeLeetcode1367.java
@@ -0,0 +1,50 @@
+package com.ujjwal_Learning.binaryTrees_2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.ujjwal_Learning.linkedList.ListNode;
+
+public class LinkedListInBinaryTreeLeetcode1367 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isSubPath(ListNode head, TreeNode root) {
+		List<TreeNode> treeList = new ArrayList<>();
+		storeTreeAsList(treeList, head, root);
+		for (TreeNode treeNode : treeList) {
+			if (checkPath(head, treeNode)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	public void storeTreeAsList(List<TreeNode> tree, ListNode listNode, TreeNode treeNode) {
+		if (listNode == null || treeNode == null) {
+			return;
+		}
+		if (listNode.val == treeNode.val) {
+			tree.add(treeNode);
+		}
+		storeTreeAsList(tree, listNode, treeNode.left);
+		storeTreeAsList(tree, listNode, treeNode.right);
+	}
+
+	public boolean checkPath(ListNode listNode, TreeNode treeNode) {
+		if (listNode == null) {
+			return true;
+		}
+		if (treeNode == null) {
+			return false;
+		}
+		if (listNode.val == treeNode.val) {
+			return checkPath(listNode.next, treeNode.left) || checkPath(listNode.next, treeNode.right);
+		}
+		return false;
+	}
+
+}
