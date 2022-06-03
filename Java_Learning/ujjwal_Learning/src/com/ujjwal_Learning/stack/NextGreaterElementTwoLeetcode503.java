92e3f73773b75bae617c8ec1d64b2df07ce995e2
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-03 21:16:34 +0530

    Updated Leetcode solution 503

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/stack/NextGreaterElementTwoLeetcode503.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/stack/NextGreaterElementTwoLeetcode503.java
@@ -9,6 +9,7 @@ public class NextGreaterElementTwoLeetcode503 {
 
 	}
 
+	// Funtion to get array of next greater elements
 	public int[] nextGreaterElements(int[] nums) {
 		int n = nums.length;
 		int[] res = new int[n];
