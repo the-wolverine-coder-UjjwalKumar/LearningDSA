576b719f24040a3943bd992c53f3d15f0e672d24
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-05 08:42:57 +0530

    Updated Leetcode solution 713

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming/SubarrayProductLessThanKLeetCode713.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming/SubarrayProductLessThanKLeetCode713.java
@@ -36,4 +36,20 @@ public class SubarrayProductLessThanKLeetCode713 {
 		return ans;
 	}
 
+	public int numSubarrayProductLessThanK1(int[] nums, int k) {
+		if (k == 0)
+			return 0;
+		int cnt = 0;
+		int pro = 1;
+		for (int i = 0, j = 0; j < nums.length; j++) {
+			pro *= nums[j];
+			while (i <= j && pro >= k) {
+				pro /= nums[i++];
+			}
+			cnt += j - i + 1;
+		}
+		return cnt;
+
+	}
+
 }
