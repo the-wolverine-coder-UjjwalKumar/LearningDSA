ae0b9139b0d8a04c46a4b440ab9793d5b8a0059a
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-07 21:58:43 +0530

    Added Leetcode solution 88

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/MergeSortedArrayLeetcode88.java
@@ -0,0 +1,30 @@
+package com.ujjwal_Learning.Arrays3;
+
+public class MergeSortedArrayLeetcode88 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public void merge(int[] A, int m, int[] B, int n) {
+		for (int i = m + n - 1; i >= 0; i--) {
+			if (m > 0 && n > 0) {
+				if (B[n - 1] > A[m - 1]) {
+					A[i] = B[n - 1];
+					n--;
+				} else {
+					A[i] = A[m - 1];
+					m--;
+				}
+			} else if (m > 0) {
+				A[i] = A[m - 1];
+				m--;
+			} else if (n > 0) {
+				A[i] = B[n - 1];
+				n--;
+			}
+		}
+	}
+
+}
