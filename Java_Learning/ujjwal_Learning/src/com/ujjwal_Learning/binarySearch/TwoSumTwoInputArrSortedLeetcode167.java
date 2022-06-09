6b280d72c03aa367573218eca402fdb7fe1b59a9
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-09 22:08:10 +0530

    Revisit Leetcode solution 167

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/TwoSumTwoInputArrSortedLeetcode167.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/TwoSumTwoInputArrSortedLeetcode167.java
@@ -7,6 +7,7 @@ public class TwoSumTwoInputArrSortedLeetcode167 {
 
 	}
 
+	// Added two sum solution
 	public int[] twoSum(int[] numbers, int target) {
 		int l = 0, r = numbers.length - 1;
 		while (numbers[l] + numbers[r] != target) {
