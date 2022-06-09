d4ce61cb5980584b4eeb667786e8a886e4f140fa
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-06-09 22:09:16 +0530

    Added Leetcode solution 173

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/BinarySearchIteratorLeetcode173.java
@@ -0,0 +1,36 @@
+package com.ujjwal_Learning.binarySearch;
+
+import java.util.Stack;
+
+import com.ujjwal_Learning.BST.TreeNode;
+
+public class BinarySearchIteratorLeetcode173 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private Stack<TreeNode> stack = new Stack<TreeNode>();
+
+	public BinarySearchIteratorLeetcode173(TreeNode root) {
+        pushAll(root);
+    }
+
+	/** @return whether we have a next smallest number */
+	public boolean hasNext() {
+		return !stack.isEmpty();
+	}
+
+	/** @return the next smallest number */
+	public int next() {
+		TreeNode tmpNode = stack.pop();
+		pushAll(tmpNode.right);
+		return tmpNode.val;
+	}
+
+	private void pushAll(TreeNode node) {
+		for (; node != null; stack.push(node), node = node.left)
+			;
+	}
+}
