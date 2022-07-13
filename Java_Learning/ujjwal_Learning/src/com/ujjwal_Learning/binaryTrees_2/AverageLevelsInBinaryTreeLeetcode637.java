25b0f0e4bf9ec18320d597480ed13bb4e19ea2e6
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-07-13 08:47:34 +0530

    Added Leetcode solution 637

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/AverageLevelsInBinaryTreeLeetcode637.java
@@ -0,0 +1,38 @@
+package com.ujjwal_Learning.binaryTrees_2;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class AverageLevelsInBinaryTreeLeetcode637 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<Double> averageOfLevels(TreeNode root) {
+		List<Double> result = new ArrayList<>();
+		Queue<TreeNode> q = new LinkedList<>();
+
+		if (root == null)
+			return result;
+		q.add(root);
+		while (!q.isEmpty()) {
+			int n = q.size();
+			double sum = 0.0;
+			for (int i = 0; i < n; i++) {
+				TreeNode node = q.poll();
+				sum += node.val;
+				if (node.left != null)
+					q.offer(node.left);
+				if (node.right != null)
+					q.offer(node.right);
+			}
+			result.add(sum / n);
+		}
+		return result;
+	}
+
+}
