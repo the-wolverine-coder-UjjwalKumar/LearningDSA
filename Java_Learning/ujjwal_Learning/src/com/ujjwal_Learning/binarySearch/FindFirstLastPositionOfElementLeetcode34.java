32f4a702d7345bb3c5e961b02425b84578949cf5
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-07-25 09:46:45 +0530

    Updated Leetcode solution 34

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/FindFirstLastPositionOfElementLeetcode34.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/FindFirstLastPositionOfElementLeetcode34.java
@@ -7,6 +7,7 @@ public class FindFirstLastPositionOfElementLeetcode34 {
 
 	}
 
+	// Leetcode daily solution 
 	public static int firstIndex(int[] arr, int data) {
 		int low = 0, high = arr.length - 1;
 		int fi = -1; // fi : first index
