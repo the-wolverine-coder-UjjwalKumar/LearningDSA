eb594f55516c9cb1de139f33beee02d52545136e
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-09-25 09:46:37 +0530

    Added Leetcode solution 766

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/matrixProblems/ToeplitzMatrixLeetcode766.java
@@ -0,0 +1,19 @@
+package com.ujjwal_Learning.matrixProblems;
+
+public class ToeplitzMatrixLeetcode766 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean isToeplitzMatrix(int[][] matrix) {
+		int m = matrix.length, n = matrix[0].length;
+		for (int i = 1; i < m; i++)
+			for (int j = 1; j < n; j++)
+				if (matrix[i][j] != matrix[i - 1][j - 1])
+					return false;
+		return true;
+	}
+
+}
