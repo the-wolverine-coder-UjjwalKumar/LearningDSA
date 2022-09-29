4f7f889b53c70da5b08a6f132803a0f68e3dfc1d
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-09-29 10:35:58 +0530

    Added leetcode solution 658

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/maths/FindKClosestElementLeetcode658.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/maths/FindKClosestElementLeetcode658.java
@@ -10,6 +10,7 @@ public class FindKClosestElementLeetcode658 {
 
 	}
 
+	// Added Leetcode solution
 	public List<Integer> findClosestElements(int[] arr, int k, int x) {
 		int lo = 0;
 		int hi = arr.length - 1;
