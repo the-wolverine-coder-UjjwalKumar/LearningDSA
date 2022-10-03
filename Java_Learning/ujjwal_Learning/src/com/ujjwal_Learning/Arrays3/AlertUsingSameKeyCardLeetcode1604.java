af762a4f54f3618cd16eeb9f71fe545f5df24f86
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-10-03 07:15:18 +0530

    Added Leetcode solution 1604

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/AlertUsingSameKeyCardLeetcode1604.java
@@ -0,0 +1,43 @@
+package com.ujjwal_Learning.Arrays3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
+public class AlertUsingSameKeyCardLeetcode1604 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public List<String> alertNames(String[] keyName, String[] keyTime) {
+		int n = keyName.length;
+		HashMap<String, List<Integer>> map = new HashMap<>();
+		List<String> ans = new ArrayList<>();
+		for (int i = 0; i < n; i++) {
+			if (!map.containsKey(keyName[i]))
+				map.put(keyName[i], new ArrayList<>());
+			String[] x = keyTime[i].split(":");
+			int time = Integer.parseInt(x[0]) * 60 + Integer.parseInt(x[1]);
+			map.get(keyName[i]).add(time);
+		}
+		Set<String> keys = map.keySet();
+		for (String s : keys) {
+			Collections.sort(map.get(s));
+			List<Integer> curr = map.get(s);
+			for (int i = 0; i < curr.size() - 2; i++) {
+				int t1 = curr.get(i), t2 = curr.get(i + 1), t3 = curr.get(i + 2);
+				if (t2 <= t1 + 60 && t3 <= t1 + 60) {
+					ans.add(s);
+					break;
+				}
+			}
+		}
+		Collections.sort(ans);
+		return ans;
+	}
+
+}
