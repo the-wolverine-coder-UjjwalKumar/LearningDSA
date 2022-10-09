7dd4286be9ee5ab6a8f37de6fdea20b70efe591c
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-10-09 11:01:03 +0530

    Updated Leetcode solution 701

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/LeetCodeQuestions/InsertNodeInBSTLeetCode701.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/LeetCodeQuestions/InsertNodeInBSTLeetCode701.java
@@ -7,7 +7,7 @@ public class InsertNodeInBSTLeetCode701 {
 
 	}
 
-	// Added Leetode daaily solution
+	// Added Leetode daily solution
 	public TreeNode insertIntoBST(TreeNode root, int val) {
 		if (root == null)
 			return new TreeNode(val);
