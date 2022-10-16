0caa3ce79818d7684e45f6665e6c1a5ee02046cf
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-10-16 12:18:22 +0530

    Added Leetcode solution `335

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/MinimumDifficultyOfJobScheduleLeetcode1335.java
@@ -0,0 +1,10 @@
+package com.ujjwal_Learning.Arrays3;
+
+public class MinimumDifficultyOfJobScheduleLeetcode1335 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
