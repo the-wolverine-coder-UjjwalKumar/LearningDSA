c0e46a427dcfd38624e013bce5c31179514ff4f4
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-10-17 07:49:23 +0530

    Added Leetcode solution 926

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/FlipStringToMonotoneIncreasingLeetcode926.java
@@ -0,0 +1,34 @@
+package com.ujjwal_Learning.strings;
+
+public class FlipStringToMonotoneIncreasingLeetcode926 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int minFlipsMonoIncr(String S) {
+		if (S == null || S.length() <= 0)
+			return 0;
+
+		char[] sChars = S.toCharArray();
+		int flipCount = 0;
+		int onesCount = 0;
+
+		for (int i = 0; i < sChars.length; i++) {
+			if (sChars[i] == '0') {
+				if (onesCount == 0)
+					continue;
+				else
+					flipCount++;
+			} else {
+				onesCount++;
+			}
+			if (flipCount > onesCount) {
+				flipCount = onesCount;
+			}
+		}
+		return flipCount;
+	}
+
+}
