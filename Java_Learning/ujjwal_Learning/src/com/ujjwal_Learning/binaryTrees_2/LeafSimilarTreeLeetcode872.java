61ac0a8f322909387e00fea7196f007946717dfd
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-12-08 08:47:48 +0530

    Added leetcode solution 872

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/LeafSimilarTreeLeetcode872.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/LeafSimilarTreeLeetcode872.java
@@ -9,6 +9,7 @@ public class LeafSimilarTreeLeetcode872 {
 
 	}
 
+	// added method to find similar leaf node values 
 	public boolean leafSimilar(TreeNode root1, TreeNode root2) {
 		Stack<TreeNode> s1 = new Stack<>(), s2 = new Stack<>();
 		s1.push(root1);
