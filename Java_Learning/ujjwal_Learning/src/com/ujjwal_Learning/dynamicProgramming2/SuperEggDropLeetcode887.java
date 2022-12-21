b302fa848631a2711fe1562acfd42b02cbc28582
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2022-12-21 21:57:49 +0530

    Updated Leetcode solution 887

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming2/SuperEggDropLeetcode887.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming2/SuperEggDropLeetcode887.java
@@ -6,5 +6,24 @@ public class SuperEggDropLeetcode887 {
 		// TODO Auto-generated method stub
 
 	}
+	
+	 public int superEggDrop2DDP(int K, int N) {
+	        int[][] dp = new int[N + 1][K + 1];
+	        int m = 0;
+	        while (dp[m][K] < N) {
+	            ++m;
+	            for (int k = 1; k <= K; ++k)
+	                dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1;
+	        }
+	        return m;
+	    }
+	 
+	 public int superEggDrop1DDP(int K, int N) {
+	        int dp[] = new int[K + 1], m = 0;
+	        for (m = 0; dp[K] < N; ++m)
+	            for (int k = K; k > 0; --k)
+	                dp[k] += dp[k - 1] + 1;
+	        return m;
+	    }
 
 }
