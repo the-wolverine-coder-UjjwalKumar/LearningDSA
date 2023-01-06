955e7b68138ed5704fe02871f109301a9d377dac
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-01-06 21:14:57 +0530

    Added leetcode solution 1833

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/maths/MaximumIceCreamBarsLeetcode1833.java
@@ -0,0 +1,23 @@
+package com.ujjwal_Learning.maths;
+
+import java.util.Arrays;
+
+public class MaximumIceCreamBarsLeetcode1833 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int maxIceCream(int[] costs, int coins) {
+		Arrays.sort(costs);
+		int count = 0;
+		for (int i : costs) {
+			if (i <= coins) {
+				count++;
+				coins -= i;
+			}
+		}
+		return count;
+	}
+}
