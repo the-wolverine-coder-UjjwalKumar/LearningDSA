1949f3ebfd2fd2311cf7b4609c98324cf4f04ae8
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-02-05 13:06:02 +0530

    Added Leetcode solution 1556

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/maths/ThousandSeperatorLeetcode1556.java
@@ -0,0 +1,34 @@
+package com.ujjwal_Learning.maths;
+
+public class ThousandSeperatorLeetcode1556 {
+
+	public static void main(String[] args) {
+		int n = 1234;
+		System.out.println(thousandSeparator(n));
+	}
+
+	public static String thousandSeparator1(int n) {
+		Integer value = n;
+		if (value < 1000)
+			return value.toString();
+
+		Integer q = n / 1000;
+		Integer r = n % 1000;
+
+		String last = r == 0 ? "0" : r.toString();
+		return q.toString() + "." + last;
+
+	}
+
+	public static String thousandSeparator(int n) {
+		String num = Integer.toString(n);
+		StringBuilder sb = new StringBuilder();
+		for (int i = num.length(); i > 0; i -= 3) {
+			if (sb.length() > 0)
+				sb.insert(0, ".");
+			sb.insert(0, num.substring(Math.max(0, i - 3), i));
+		}
+		return sb.toString();
+	}
+
+}
