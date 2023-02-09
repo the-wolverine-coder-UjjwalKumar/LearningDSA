dcfb4973a6191c042d65a6d2d5890effb99fea41
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-02-09 07:06:11 +0530

    Added leetcode solution 2306

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/NameOfCompaniesLeetcode2306.java
@@ -0,0 +1,29 @@
+package com.ujjwal_Learning.strings;
+
+import java.util.HashSet;
+
+public class NameOfCompaniesLeetcode2306 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}public long distinctNames(String[] ideas) {
+        HashSet<Integer>[] count = new HashSet[26];
+        for (int i = 0; i < 26; ++i)
+            count[i] = new HashSet<>();
+        for (String s : ideas)
+            count[s.charAt(0) - 'a'].add(s.substring(1).hashCode());
+        long res = 0;
+        for (int i = 0; i < 26; ++i)
+            for (int j = i + 1; j < 26; ++j) {
+                long c1 = 0, c2 = 0;
+                for (int c : count[i])
+                    if (!count[j].contains(c)) c1++;
+                for (int c : count[j])
+                    if (!count[i].contains(c)) c2++;
+                res += c1 * c2;
+            }
+        return res * 2;
+    }
+
+}
