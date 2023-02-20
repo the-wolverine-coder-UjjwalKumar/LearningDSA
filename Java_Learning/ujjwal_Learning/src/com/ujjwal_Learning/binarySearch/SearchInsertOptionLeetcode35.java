c8f886d0b609df42bd38270bfbc67ff7aa6c66c2
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-02-20 21:29:14 +0530

    Added Leetcode solution 35

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/SearchInsertOptionLeetcode35.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binarySearch/SearchInsertOptionLeetcode35.java
@@ -7,6 +7,7 @@ public class SearchInsertOptionLeetcode35 {
 
 	}
 
+	// search insert position using BS
 	public int searchInsert(int[] nums, int target) {
 		int start = 0, end = nums.length - 1;
 		while (start <= end) {
