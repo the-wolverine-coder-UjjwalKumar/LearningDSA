9928c920e5b0eb4f9687477ea9da669af6cdf1ce
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-03-05 15:22:17 +0530

    Added Leetcode solution 1871

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/JumpGame4Leetcode1871.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/JumpGame4Leetcode1871.java
@@ -1,6 +1,8 @@
 package com.ujjwal_Learning.Arrays3;
 
+import java.util.HashMap;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 
 public class JumpGame4Leetcode1871 {
@@ -10,7 +12,34 @@ public class JumpGame4Leetcode1871 {
 
 	}
 
-	// Daily Leetcode solution
+	// added BFS solution to find the optimal jumps to reach destination
+	public int minJumps(int[] arr) {
+        int n = arr.length;
+        HashMap<Integer, List<Integer>> indicesOfValue = new HashMap<>();
+        for (int i = 0; i < n; i++)
+            indicesOfValue.computeIfAbsent(arr[i], x -> new LinkedList<>()).add(i);
+        boolean[] visited = new boolean[n]; visited[0] = true;
+        Queue<Integer> q = new LinkedList<>(); q.offer(0);
+        int step = 0;
+        while (!q.isEmpty()) {
+            for (int size = q.size(); size > 0; --size) {
+                int i = q.poll();
+                if (i == n - 1) return step; // Reached to last index
+                List<Integer> next = indicesOfValue.get(arr[i]);
+                next.add(i - 1); next.add(i + 1);
+                for (int j : next) {
+                    if (j >= 0 && j < n && !visited[j]) {
+                        visited[j] = true;
+                        q.offer(j);
+                    }
+                }
+                next.clear(); // avoid later lookup indicesOfValue arr[i]
+            }
+            step++;
+        }
+        return 0;
+    }
+	
 	public boolean canReach(String s, int min, int max) {
 		if (s.charAt(s.length() - 1) == '1')
 			return false;
