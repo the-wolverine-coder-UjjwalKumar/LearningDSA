3fdf4c9eea768cf73990c5400eabdd89dc608f09
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-03-25 21:55:38 +0530

    Added Leetcode solution 2316

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/graph/CountUnreachablepairsOfNodeInUGLeetcode2316.java
@@ -0,0 +1,10 @@
+package com.ujjwal_Learning.graph;
+
+public class CountUnreachablepairsOfNodeInUGLeetcode2316 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
