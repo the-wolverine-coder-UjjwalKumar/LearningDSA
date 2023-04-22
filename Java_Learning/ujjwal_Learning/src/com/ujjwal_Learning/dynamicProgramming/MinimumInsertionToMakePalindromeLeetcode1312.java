f5f762fd5588f6c6f1a9b3ec4eb038b1c6ae7ee6
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-04-22 21:19:05 +0530

    Added Leetcode solution 1312

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming/MinimumInsertionToMakePalindromeLeetcode1312.java
@@ -0,0 +1,20 @@
+package com.ujjwal_Learning.dynamicProgramming;
+
+public class MinimumInsertionToMakePalindromeLeetcode1312 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int minInsertions(String s) {
+		int n = s.length();
+		int[][] dp = new int[n + 1][n + 1];
+		for (int i = 0; i < n; ++i)
+			for (int j = 0; j < n; ++j)
+				dp[i + 1][j + 1] = s.charAt(i) == s.charAt(n - 1 - j) ? dp[i][j] + 1
+						: Math.max(dp[i][j + 1], dp[i + 1][j]);
+		return n - dp[n][n];
+	}
+
+}
