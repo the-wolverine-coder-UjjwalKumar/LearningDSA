5eea01b6f672b8f7d38ab099ea42890be99868d5
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-04-23 14:07:24 +0530

    Added Leetcode solution 1416

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming2/RestoreTheArrayLeetcode1416.java
@@ -0,0 +1,36 @@
+package com.ujjwal_Learning.dynamicProgramming2;
+
+public class RestoreTheArrayLeetcode1416 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int numberOfArrays(String s, int k) {
+		Integer[] dp = new Integer[s.length()]; // dp[i] is number of ways to print valid arrays from string s start at
+												// i
+		return dfs(s, k, 0, dp);
+	}
+
+	int dfs(String s, long k, int i, Integer[] dp) {
+		if (i == s.length())
+			return 1; // base case -> Found a valid way
+		if (s.charAt(i) == '0')
+			return 0; // all numbers are in range [1, k] and there are no leading zeros -> So numbers
+						// starting with 0 mean invalid!
+		if (dp[i] != null)
+			return dp[i];
+		int ans = 0;
+		long num = 0;
+		for (int j = i; j < s.length(); j++) {
+			num = num * 10 + s.charAt(j) - '0'; // num is the value of the substring s[i..j]
+			if (num > k)
+				break; // num must be in range [1, k]
+			ans += dfs(s, k, j + 1, dp);
+			ans %= 1_000_000_007;
+		}
+		return dp[i] = ans;
+	}
+
+}
