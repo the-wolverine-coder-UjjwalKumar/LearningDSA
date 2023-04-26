3095a99ac57e10f221e79bfa7ca0a0ca1f5f5934
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-04-26 08:52:23 +0530

    Added solution

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/numberTheory/AddDigitsLeetCode258.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/numberTheory/AddDigitsLeetCode258.java
@@ -7,7 +7,7 @@ public class AddDigitsLeetCode258 {
 		System.out.println(addDigits(0));
 	}
 
-	// add digits till it become one signle digit
+	// add digits till it become one single digit
 	public static int addDigits(int num) {
 
 		if (num <= 9)
