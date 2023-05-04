c3b5a7ce32e1509b6b328e635ed337582ccce049
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-05-04 08:48:19 +0530

    Added Leetcode solution 649

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Games/Dota2SenateLeetcode649.java
@@ -0,0 +1,44 @@
+package com.ujjwal_Learning.Games;
+
+public class Dota2SenateLeetcode649 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String predictPartyVictory(String senate) {
+		int r = 0, d = 0, start = 0;
+		char[] arr = senate.toCharArray();
+		for (char c : arr) {
+			if (c == 'R')
+				r++;
+			else
+				d++;
+		}
+
+		while (r > 0 && d > 0) {
+			while (arr[start] != 'R' && arr[start] != 'D') {
+				start = (start + 1) % arr.length;
+			}
+
+			char ban = 'R';
+			if (arr[start] == 'R') {
+				ban = 'D';
+				d--;
+			} else {
+				r--;
+			}
+			int idx = (start + 1) % arr.length;
+			while (arr[idx] != ban) {
+				idx = (idx + 1) % arr.length;
+			}
+
+			arr[idx] = ' ';
+			start = (start + 1) % arr.length;
+		}
+
+		return d == 0 ? "Radiant" : "Dire";
+	}
+
+}
