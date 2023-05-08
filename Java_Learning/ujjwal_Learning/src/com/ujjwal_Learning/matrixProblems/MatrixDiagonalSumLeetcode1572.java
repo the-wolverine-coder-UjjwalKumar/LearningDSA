c4dbdecc6cafa4f1f9e53c702d7ec3dbc5513ff1
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-05-08 08:54:53 +0530

    Added Leetcode solution 1572

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/matrixProblems/MatrixDiagonalSumLeetcode1572.java
@@ -0,0 +1,25 @@
+package com.ujjwal_Learning.matrixProblems;
+
+public class MatrixDiagonalSumLeetcode1572 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int diagonalSum(int[][] mat) {
+		int n = mat[0].length - 1;
+		int sum = 0;
+		for (int row = 0; row < mat.length; row++) {
+
+			if (row != (n - row))
+				sum += mat[row][row] + mat[row][n - row];
+			else
+				sum += mat[row][row];
+
+		}
+
+		return sum;
+	}
+
+}
