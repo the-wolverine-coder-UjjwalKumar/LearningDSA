2c00f40ce271ddb65a237c2ae1e48944ea441b2f
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-05-13 21:32:13 +0530

    Added Leetcode solution 2466

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming/CountNumberOfWaysToHaveGoodStringLeetcode2466.java
@@ -0,0 +1,24 @@
+package com.ujjwal_Learning.dynamicProgramming;
+
+public class CountNumberOfWaysToHaveGoodStringLeetcode2466 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int countGoodStrings(int low, int high, int zero, int one) {
+		int dp[] = new int[high + 1], res = 0, mod = 1000000007;
+		dp[0] = 1;
+		for (int i = 1; i <= high; ++i) {
+			if (i >= zero)
+				dp[i] = (dp[i] + dp[i - zero]) % mod;
+			if (i >= one)
+				dp[i] = (dp[i] + dp[i - one]) % mod;
+			if (i >= low)
+				res = (res + dp[i]) % mod;
+		}
+		return res;
+	}
+
+}
