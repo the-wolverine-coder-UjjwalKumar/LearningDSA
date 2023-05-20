1f846ed0b6fba10deccf94684375f21c9d275a31
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-05-20 22:41:06 +0530

    Added Leetcode solution 1103

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/DistributeCandiestoPeopleLeetcode1103.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/DistributeCandiestoPeopleLeetcode1103.java
@@ -1,5 +1,8 @@
 package com.ujjwal_Learning.Arrays3;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class DistributeCandiestoPeopleLeetcode1103 {
 
 	public static void main(String[] args) {
@@ -14,5 +17,11 @@ public class DistributeCandiestoPeopleLeetcode1103 {
 		}
 		return people;
 	}
+	
+	public int distributeCandies(int[] candies) {
+        Set<Integer> kinds = new HashSet<>();
+        for (int candy : candies) kinds.add(candy);
+        return kinds.size() >= candies.length / 2 ? candies.length / 2 : kinds.size();
+    }
 
 }
