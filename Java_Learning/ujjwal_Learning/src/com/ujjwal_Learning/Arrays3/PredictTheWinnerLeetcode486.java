085a8905fef7854131eb9a1b2bca95a1e7d06257
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-07-28 09:43:23 +0530

    Added Leetcode solution 486

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/PredictTheWinnerLeetcode486.java
@@ -0,0 +1,21 @@
+package com.ujjwal_Learning.Arrays3;
+
+public class PredictTheWinnerLeetcode486 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public boolean PredictTheWinner(int[] nums) {
+		return helper(nums, 0, nums.length - 1, new Integer[nums.length][nums.length]) >= 0;
+	}
+
+	private int helper(int[] nums, int s, int e, Integer[][] mem) {
+		if (mem[s][e] == null)
+			mem[s][e] = s == e ? nums[e]
+					: Math.max(nums[e] - helper(nums, s, e - 1, mem), nums[s] - helper(nums, s + 1, e, mem));
+		return mem[s][e];
+	}
+
+}
