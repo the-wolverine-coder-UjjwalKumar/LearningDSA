acac2315a18c733fc2ce96fab11db87c1bb7df52
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-08-06 20:25:19 +0530

    Added Leetcode solution 920

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming2/NumberOfMusicPlaylistLeetcode920.java
@@ -0,0 +1,27 @@
+package com.ujjwal_Learning.dynamicProgramming2;
+
+public class NumberOfMusicPlaylistLeetcode920 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	long mod = (long) 1e9 + 7;
+
+	public int numMusicPlaylists(int N, int L, int K) {
+		long[][] dp = new long[N + 1][L + 1];
+		for (int i = K + 1; i <= N; ++i)
+			for (int j = i; j <= L; ++j)
+				if ((i == j) || (i == K + 1))
+					dp[i][j] = factorial(i);
+				else
+					dp[i][j] = (dp[i - 1][j - 1] * i + dp[i][j - 1] * (i - K)) % mod;
+		return (int) dp[N][L];
+	}
+
+	long factorial(int n) {
+		return n > 0 ? factorial(n - 1) * n % mod : 1;
+	}
+
+}
