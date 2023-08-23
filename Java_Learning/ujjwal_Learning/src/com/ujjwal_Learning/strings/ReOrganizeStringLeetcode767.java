950d1d2c1adf36045893d42783694c94310ba581
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-08-23 09:18:06 +0530

    Added Leetcode solution 767

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/ReOrganizeStringLeetcode767.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/ReOrganizeStringLeetcode767.java
@@ -1,11 +1,15 @@
 package com.ujjwal_Learning.strings;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.PriorityQueue;
 
 public class ReOrganizeStringLeetcode767 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		String ans = reorganizeStringN("aab");
+		System.out.println(ans);
 
 	}
 
@@ -41,6 +45,75 @@ public class ReOrganizeStringLeetcode767 {
 		}
 
 		return hold == null ? sb.toString() : "";
+	}
+	
+	private static String reorganizeStringN(String inputS) {
+		StringBuilder sb = new StringBuilder();
+		
+		Map<Character,Integer> hashMap = new HashMap<>();
+		
+		for (int i = 0; i < inputS.length(); i++) {
+			int cnt = hashMap.getOrDefault(inputS.charAt(i), 1);
+			hashMap.put(inputS.charAt(i), (cnt+1));
+		}
+		
+		PriorityQueue<Pair> maxHeap = new PriorityQueue<>((a, b) -> -(a.getCnt() - b.getCnt()));
+		
+		for (Map.Entry<Character,Integer> entry : hashMap.entrySet()) {
+			Pair record = new Pair(entry.getKey(), entry.getValue());
+			maxHeap.add(record);
+		}
+		
+		// Now 
+		Pair blocked;
+		Pair currItem;
+		
+		blocked = maxHeap.poll();
+		
+		sb.append(blocked.getCh());
+		blocked.setCnt((blocked.getCnt()-1));
+		
+		while (maxHeap.size() > 0) {
+			currItem = maxHeap.poll();
+            sb.append(currItem.getCh());
+            currItem.setCnt((currItem.getCnt()-1));
+
+            if (blocked.getCnt()>0)
+                maxHeap.add(blocked);
+            blocked = currItem;
+		}
+		
+		if (blocked.getCnt()!=0 && maxHeap.size() == 0) {
+			return "";
+		}
+		
+		return sb.toString();
+	}
+	
+	static class Pair {
+		char ch;
+		int cnt;
+		
+		public Pair(char ch, int cnt) {
+			this.ch = ch;
+			this.cnt = cnt;
+		}
+		
+		public char getCh() {
+			return this.ch;
+		}
+		
+		public int getCnt() {
+			return this.cnt;
+		}
+		
+		public void setCh(char ch) {
+			this.ch = ch;
+		}
+		
+		public void setCnt(int cnt) {
+			this.cnt = cnt;
+		}
 	}
 
 }
