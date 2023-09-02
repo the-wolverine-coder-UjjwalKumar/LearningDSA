b11c7ea1b8d225acd5e34d35fda78c748f5805e6
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-09-02 20:54:58 +0530

    Added leetcode solution 2707

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming2/ExtraCharacterInAStringLeetcode2707.java
@@ -0,0 +1,35 @@
+package com.ujjwal_Learning.dynamicProgramming2;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class ExtraCharacterInAStringLeetcode2707 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int minExtraChar(String s, String[] dictionary) {
+		int maxLen = s.length() + 1;
+		int[] dp = new int[maxLen];
+		Arrays.fill(dp, maxLen);
+
+		dp[0] = 0;
+		Set<String> wordSet = new HashSet<>(Arrays.asList(dictionary));
+
+		for (int i = 1; i < maxLen; i++) {
+			dp[i] = dp[i - 1] + 1;
+
+			for (int length = 1; length <= i; length++) {
+				String substring = s.substring(i - length, i);
+				if (wordSet.contains(substring)) {
+					dp[i] = Math.min(dp[i], dp[i - length]);
+				}
+			}
+		}
+		return dp[maxLen - 1];
+	}
+
+}
