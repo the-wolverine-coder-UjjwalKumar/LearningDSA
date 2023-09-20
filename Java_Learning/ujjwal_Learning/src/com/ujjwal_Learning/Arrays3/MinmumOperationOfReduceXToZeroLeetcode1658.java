d21d10bcda8b66f9e3e93dae863ec5533504146e
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-09-20 10:00:21 +0530

    Added Leetcode solution 1658

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/MinmumOperationOfReduceXToZeroLeetcode1658.java
@@ -0,0 +1,34 @@
+package com.ujjwal_Learning.Arrays3;
+
+public class MinmumOperationOfReduceXToZeroLeetcode1658 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int minOperations(int[] nums, int x) {
+		int target = -x, n = nums.length;
+		for (int num : nums)
+			target += num;
+
+		if (target == 0)
+			return n;
+
+		int maxLen = 0, curSum = 0, left = 0;
+
+		for (int right = 0; right < n; ++right) {
+			curSum += nums[right];
+			while (left <= right && curSum > target) {
+				curSum -= nums[left];
+				left++;
+			}
+			if (curSum == target) {
+				maxLen = Math.max(maxLen, right - left + 1);
+			}
+		}
+
+		return maxLen != 0 ? n - maxLen : -1;
+	}
+
+}
