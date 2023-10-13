22236f72d6d35bb0a57bdc7c043987a5885799ff
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-10-13 09:02:46 +0530

    Added Leetcode solution 746

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming2/MostClimbingStairsLeetcode746.java
@@ -0,0 +1,29 @@
+package com.ujjwal_Learning.dynamicProgramming2;
+
+public class MostClimbingStairsLeetcode746 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	int[] dp;
+
+	public int minCostClimbingStairs(int[] cost) {
+		int n = cost.length;
+		dp = new int[n];
+		return Math.min(minCost(cost, n - 1), minCost(cost, n - 2));
+	}
+
+	private int minCost(int[] cost, int n) {
+		if (n < 0)
+			return 0;
+		if (n == 0 || n == 1)
+			return cost[n];
+		if (dp[n] != 0)
+			return dp[n];
+		dp[n] = cost[n] + Math.min(minCost(cost, n - 1), minCost(cost, n - 2));
+		return dp[n];
+	}
+
+}
