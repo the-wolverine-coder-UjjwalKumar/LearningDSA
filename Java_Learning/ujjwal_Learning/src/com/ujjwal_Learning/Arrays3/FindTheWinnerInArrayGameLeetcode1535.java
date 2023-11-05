30928f4dc223532e4af12644151cb4e7f2cc36db
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-11-05 21:01:40 +0530

    Added Leetcode solution 1535

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/FindTheWinnerInArrayGameLeetcode1535.java
@@ -0,0 +1,22 @@
+package com.ujjwal_Learning.Arrays3;
+
+public class FindTheWinnerInArrayGameLeetcode1535 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int getWinner(int[] A, int k) {
+		int cur = A[0], win = 0;
+		for (int i = 1; i < A.length; ++i) {
+			if (A[i] > cur) {
+				cur = A[i];
+				win = 0;
+			}
+			if (++win == k)
+				break;
+		}
+		return cur;
+	}
+}
