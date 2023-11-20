d42db9a9e29f52b06e126272a67aa68d1e628ef1
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2023-11-20 21:11:39 +0530

    Added Leetcode solution 2391

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/strings/MinimumTimeToCoolectGarbageLeetcode2391.java
@@ -0,0 +1,42 @@
+package com.ujjwal_Learning.strings;
+
+public class MinimumTimeToCoolectGarbageLeetcode2391 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int garbageCollection(String[] garbage, int[] travel) {
+		int sum = 0, mul = 0;
+		int p = 0, g = 0, m = 0;
+		for (int i = garbage.length - 1; i > 0; i--) {
+			sum += garbage[i].length();
+			if (mul < 3) {
+				for (int j = 0; j < garbage[i].length(); j++) {
+					char ch = garbage[i].charAt(j);
+					switch (ch) {
+					case 'P':
+						mul = p == 0 ? mul + 1 : mul;
+						p++;
+						break;
+					case 'G':
+						mul = g == 0 ? mul + 1 : mul;
+						g++;
+						break;
+					case 'M':
+						mul = m == 0 ? mul + 1 : mul;
+						m++;
+						break;
+					default:
+						break;
+					}
+				}
+			}
+			sum += (mul * travel[i - 1]);
+		}
+		sum += garbage[0].length();
+		return sum;
+	}
+
+}
