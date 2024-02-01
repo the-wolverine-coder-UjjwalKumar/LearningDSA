2a7d2eade39b7061f9749d7a06aeed460938a541
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-02-01 20:03:03 +0530

    Added solution 2966

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays2/DivideArraysIntoMaxDifferenceLeetcode2966.java
@@ -0,0 +1,28 @@
+package com.ujjwal_Learning.Arrays2;
+
+import java.util.Arrays;
+
+public class DivideArraysIntoMaxDifferenceLeetcode2966 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int[][] divideArray(int[] nums, int k) {
+		int len = nums.length;
+		int[][] res = new int[len / 3][3];
+		Arrays.sort(nums);
+		for (int i = 0, m = 0; i < len; i = i + 3, m++) {
+			res[m][0] = nums[i];
+			for (int j = 1; j < 3; j++) {
+				if (nums[i + j] - res[m][0] <= k) {
+					res[m][j] = nums[i + j];
+				} else {
+					return new int[0][0];
+				}
+			}
+		}
+		return res;
+	}
+}
