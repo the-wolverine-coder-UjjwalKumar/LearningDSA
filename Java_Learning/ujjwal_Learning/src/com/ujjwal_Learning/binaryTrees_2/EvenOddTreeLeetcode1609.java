b968da2400cfa88dc99ba151a0eb30b59a7786ab
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-02-29 21:10:31 +0530

    Added solution for even odd tree

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/EvenOddTreeLeetcode1609.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/binaryTrees_2/EvenOddTreeLeetcode1609.java
@@ -2,8 +2,10 @@ package com.ujjwal_Learning.binaryTrees_2;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class EvenOddTreeLeetcode1609 {
 
@@ -50,6 +52,53 @@ public class EvenOddTreeLeetcode1609 {
 			levelOrderTraversal(node.left, map, level + 1);
 			levelOrderTraversal(node.right, map, level + 1);
 		}
+	}
+
+	public static boolean isEvenOddTree(TreeNode root) {
+		if (root == null)
+			return true;
+
+		Queue<TreeNode> queue = new LinkedList<>();
+		queue.add(root);
+		int level = 0;
+
+		while (!queue.isEmpty()) {
+			int size = queue.size();
+			int val;
+			if (level % 2 == 0) {
+				val = Integer.MIN_VALUE;
+			} else {
+				val = Integer.MAX_VALUE;
+			}
+			for (int i = 0; i < size; i++) {
+				TreeNode currNode = queue.poll();
+
+				if (currNode != null) {
+					if (level % 2 == 0) {
+						// even level , odd number & inc order
+						if (currNode.val % 2 == 0 || val >= currNode.val)
+							return false;
+
+					} else {
+						// odd level , even number & dec order
+						if (currNode.val % 2 != 0 || val <= currNode.val)
+							return false;
+					}
+
+					val = currNode.val;
+
+					if (currNode.left != null)
+						queue.add(currNode.left);
+					if (currNode.right != null)
+						queue.add(currNode.right);
+				}
+
+			}
+			level++;
+		}
+
+		return true;
+
 	}
 
 }
