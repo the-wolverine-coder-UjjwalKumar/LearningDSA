f5f944de590c0cd74a112a21599fdf857cd19c34
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-03-20 10:00:04 +0530

    Added solution 1669

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/MergeInBetweenLinkedListLeetcode1669.java
@@ -0,0 +1,81 @@
+package com.ujjwal_Learning.linkedList;
+
+public class MergeInBetweenLinkedListLeetcode1669 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public ListNode mergeInBetween1(ListNode list1, int a, int b, ListNode list2) {
+		ListNode prevA = null;
+		ListNode nextB = null;
+		int aCount = 0;
+		int bCount = 0;
+
+		ListNode temp = list1;
+		while (bCount <= b) {
+
+			if (aCount == a - 1) {
+				prevA = temp;
+			}
+
+			if (bCount == b) {
+				nextB = temp.next;
+			}
+
+			if (prevA != null && nextB != null)
+				break;
+
+			aCount++;
+			bCount++;
+
+			temp = temp.next;
+		}
+
+		ListNode list2lastNode = null;
+		ListNode temp2 = list2;
+		while (temp2.next != null) {
+			temp2 = temp2.next;
+		}
+
+		if (prevA != null) {
+			prevA.next = list2;
+		}
+
+		if (nextB != null) {
+			temp2.next = nextB;
+		}
+
+		return list1;
+
+	}
+
+	public ListNode mergeInBetween(ListNode list1, int a, int b, ListNode list2) {
+		ListNode first = list1;
+		ListNode second = list1;
+		ListNode ans = first;
+		int i = 1;
+		int j = 0;
+		while (i < a) {
+			first = first.next;
+			i++;
+		}
+		while (j < b) {
+			second = second.next;
+			j++;
+		}
+		first.next = list2;
+		ListNode secon = list2;
+		while (secon.next != null) {
+			secon = secon.next;
+		}
+		secon.next = second.next;
+
+		// System.out.println(first.next.val);
+		// System.out.println(second.val);
+
+		return ans;
+	}
+
+}
