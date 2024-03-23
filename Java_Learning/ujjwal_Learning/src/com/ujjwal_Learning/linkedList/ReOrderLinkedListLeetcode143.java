c36fcf06e37ce07b53d675a8fbda3b6144a849a3
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-03-23 22:02:01 +0530

    Added solution for reordering the linked list

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/ReOrderLinkedListLeetcode143.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/ReOrderLinkedListLeetcode143.java
@@ -33,5 +33,45 @@ public class ReOrderLinkedListLeetcode143 {
 		}
 		temp.next = null;// to remove the cycle or the cyclic dependency of the elements
 	}
+	
+	public void reorderList1(ListNode head) {
+        if(head.next != null && head != null) {
+
+            ListNode slow = head;
+            ListNode fast = head.next;
+
+            while(fast.next != null && fast.next.next != null){
+                slow = slow.next;
+                fast = fast.next.next;
+            }
+
+
+            if(fast.next != null){
+                slow = slow.next;
+            }
+
+            ListNode temp = slow.next;
+            ListNode newHead = temp;
+            slow.next = null;
+
+            newHead = reverse(newHead, null);
+
+            ListNode curr = head;
+
+            while(newHead != null){
+                ListNode node = curr.next;
+                curr.next = newHead;
+                newHead = node;
+                curr = curr.next;
+            }
+        }
+    }
+	
+    public ListNode reverse(ListNode head, ListNode prev){
+        if(head == null) return prev;
+        ListNode newHead = head.next;
+        head.next = prev;
+        return reverse(newHead, head);
+    }
 
 }
