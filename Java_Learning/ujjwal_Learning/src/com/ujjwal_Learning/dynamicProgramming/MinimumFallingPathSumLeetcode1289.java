c55d0d3831accc4502dea272dde9dd5730774cbe
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-04-26 21:52:27 +0530

    Added solution for minimum falling path sum

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/dynamicProgramming/MinimumFallingPathSumLeetcode1289.java
@@ -0,0 +1,44 @@
+package com.ujjwal_Learning.dynamicProgramming;
+
+import java.util.Arrays;
+
+public class MinimumFallingPathSumLeetcode1289 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public int minFallingPathSum(int[][] grid) {
+		int n = grid.length, res = Integer.MAX_VALUE;
+		int[][] dp = new int[n][n];
+		for (int row[] : dp) {
+			Arrays.fill(row, -1);
+		}
+
+		for (int j = 0; j < n; ++j) {
+			dp[0][j] = grid[0][j];
+		}
+
+		for (int i = 1; i < n; ++i) {
+			for (int j = 0; j < n; ++j) {
+				int temp = Integer.MAX_VALUE;
+
+				for (int k = 0; k < n; ++k) {
+					if (j != k) {
+						temp = Math.min(temp, grid[i][j] + dp[i - 1][k]);
+					}
+
+					dp[i][j] = temp;
+				}
+			}
+		}
+
+		for (int j = 0; j < n; ++j) {
+			res = Math.min(res, dp[n - 1][j]);
+		}
+
+		return res;
+	}
+
+}
