d8ea0ce60e3d93f9b45d2dd27355bc136b403431
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-05-07 21:51:13 +0530

    updated

--- Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/DoubleLinkedListNodeValueLeetcode2816.java
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/linkedList/DoubleLinkedListNodeValueLeetcode2816.java
@@ -6,5 +6,16 @@ public class DoubleLinkedListNodeValueLeetcode2816 {
 		// TODO Auto-generated method stub
 
 	}
+	
+	public ListNode doubleIt(ListNode head) {
+	       if (head.val > 4)
+	            head = new ListNode(0, head);
+	        for(ListNode node = head; node != null; node = node.next) {
+	            node.val = (node.val * 2) % 10;
+	            if (node.next != null && node.next.val > 4)
+	                node.val++;
+	        }
+	        return head;
+	    }
 
 }
