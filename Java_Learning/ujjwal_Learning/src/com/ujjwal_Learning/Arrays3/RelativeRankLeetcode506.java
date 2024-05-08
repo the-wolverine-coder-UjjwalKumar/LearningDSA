90de0c2e7e923d48d006c69991e2270c8cc0d109
the-wolverine-coder-UjjwalKumar <uk9334336553@gmail.com>
2024-05-08 09:33:21 +0530

    Added solution 506

---
+++ Java_Learning/ujjwal_Learning/src/com/ujjwal_Learning/Arrays3/RelativeRankLeetcode506.java
@@ -0,0 +1,34 @@
+package com.ujjwal_Learning.Arrays3;
+
+import java.util.Arrays;
+
+public class RelativeRankLeetcode506 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public String[] findRelativeRanks(int[] score) {
+		int n = score.length;
+		int[] sortedScore = score.clone();
+		Arrays.sort(sortedScore);
+		String[] ranks = new String[n];
+
+		for (int i = 0; i < n; i++) {
+			int rank = Arrays.binarySearch(sortedScore, score[i]);
+			if (rank == n - 1) {
+				ranks[i] = "Gold Medal";
+			} else if (rank == n - 2) {
+				ranks[i] = "Silver Medal";
+			} else if (rank == n - 3) {
+				ranks[i] = "Bronze Medal";
+			} else {
+				ranks[i] = String.valueOf(n - rank);
+			}
+		}
+
+		return ranks;
+	}
+
+}
